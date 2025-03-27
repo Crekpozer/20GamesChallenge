2e477b68253417e38e1c376155dde31c5bb39720
Crekpozer <ereck@live.com>
2025-03-26 21:23:57 -0300

    Day one patch

--- 1-pong/Scripts/AudioManager.gd
+++ 1-pong/Scripts/AudioManager.gd
@@ -1,13 +1,37 @@
 extends Node
 
-# Onready references to AudioStreamPlayers
-@onready var mainMenuPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
-@onready var gameplayLoopPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
-@onready var defeatedPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
-@onready var SFXPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
-@onready var victoryPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
+# Esse é um sistema simples de gerenciamento de audio, a função dele é 
+# organizar os arquivos e tocar o som pedido no momento em que for pedido
+#
+# Para tocar um audio é necessario:
+# 1 - Carregar os arquivos
+# 2 - Criar um AudioStreamPlayer para reproduzir o som
+# 3 - No _ready, adicionar o AudioStreamPlayer a arvore de nodos
+# 4 - Atribuir o arquivo de audio ao AudioStreamPlayer
+# 5 - Configurar em que bus vai qual audio
+#
+# Funções
+#
+# PARA TOCAR UMA MUSICA DE FUNDO:
+# Para tocar uma música de fundo é necessario chamar a função PlayBGMusic com uma string com nome
+# da musica, ela parará qualquer outra música de fundo que esteja tocando e tocará a 
+# música pedida.
+#
+# PARA TOCAR UM EFEITO SONORO:
+# Para tocar um efeito sonoro é preciso chamar a função PlaySFX com o nome do efeito e ele rocará
+# o efeito pedido.
+#
+# É necessario adicionar os "nomes" dos arquivos no match das funções PlayBGMusic e PlaySFX 
+# para que ele toque corretamente.
+#
+# AJUSTAR VOLUME:
+# Há uma função para ajustar o volume, mas no momento ela está com o trabalho em progresso
+# e vai ser melhorada no futuro, mas já é um bom começo.
+#
+# PARA, PARA, PARA:
+# Há uma função para parar todos os sons, a função StopAll.
 
-# Load the audio files
+# 1 - Carregar os arquivos
 var mainMenuAudio = preload("res://Audio/Track1_MainMenu.wav")
 var gameplayLoopAudio = preload("res://Audio/Track2_Gameplay.wav")
 var victoryAudio = preload("res://Audio/Track4_Victory.wav")
@@ -15,26 +39,29 @@ var defeatedAudio = preload("res://Audio/Track3_Defeated.wav")
 var ballHitSFX = preload("res://Audio/SFX1_BallHit.wav")
 var youLoseSFX = preload("res://Audio/SFX2_YouLose.wav")
 
-
-# Actual state of the song
-var isLooping = false
+# 2 - Criar um AudioStreamPlayer para reproduzir o som
+@onready var mainMenuPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var gameplayLoopPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var defeatedPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var SFXPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var victoryPlayer: AudioStreamPlayer = AudioStreamPlayer.new()
 
 # Function to initialize the AudioManager
 func _ready() -> void:
-	# Add AudioStreamPlayers to the tree
+	# 3 - No _ready, adicionar o AudioStreamPlayer a arvore de nodos
 	add_child(mainMenuPlayer)
 	add_child(gameplayLoopPlayer)
 	add_child(defeatedPlayer)
 	add_child(SFXPlayer)
 	add_child(victoryPlayer)
-
-	# Initialize the audio streams
+	
+	# 4 - Atribuir o arquivo de audio ao AudioStreamPlayer
 	mainMenuPlayer.stream = mainMenuAudio
 	gameplayLoopPlayer.stream = gameplayLoopAudio
 	defeatedPlayer.stream = defeatedAudio
 	victoryPlayer.stream = victoryAudio
 
-	# Configure where each player goes in audio bus
+	# 5 - Configurar em que bus vai qual audio
 	SetBus(mainMenuPlayer, "BGMusic")
 	SetBus(gameplayLoopPlayer, "BGMusic")
 	SetBus(defeatedPlayer, "Defeated")
--- 1-pong/Scripts/ball.gd
+++ 1-pong/Scripts/ball.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+# A Bola
+# Esse é o script da bola
+
+
 var windowSize : Vector2 # O tamanho da janela
 const startSpeed : int = 500 # A velocidade inicial da bola
 const maxYVector : float = 0.8 
@@ -7,10 +11,13 @@ var accel : int = 50 # O quanto a bola acelera
 var speed : int # A velocidade atual da bola
 var direction : Vector2 # A direção da bola
 
+# Essas variaveis alteram o código para que a bola se comporte da forma correta
+# tanto dentro do loading como no modo multiplayer
 @export var inLoading : bool = false
 @export var isMuliplayer : bool = false
 
 func _ready():
+	# Detecta o tamanho da tela
 	windowSize = get_viewport_rect().size
 
 # Função que randomiza a posição e a direção inicial da bola
@@ -21,14 +28,19 @@ func NewBall():
 	direction = RandomDirection()
 	visible = true
 
+# Função que randomiza a direção da bola
 func RandomDirection():
 	var newDirection : Vector2
 	newDirection.x = [1, -1].pick_random()
 	newDirection.y = randf_range(-1, 1)
 	return newDirection.normalized()
 
+# Função que trabalha com a fisica
 func _physics_process(delta: float) -> void:
+	
+	# Move e colide o objeto com outros objetos
 	var collision = move_and_collide(direction * speed * delta)
+	# Variavel que vai armazenar o objeto 
 	var collider
 	if collision:
 		%BallAnimationPlayer.play("Bounce")
@@ -53,6 +65,7 @@ func _physics_process(delta: float) -> void:
 		else:
 				direction = direction.bounce(collision.get_normal())
 
+# Gera uma nova direção aleatório para a bola
 func NewDirection(collider):
 	var ballY = position.y
 	var padY = collider.position.y
